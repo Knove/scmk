709f9f46c69538fd2e5299df8226468ffea4e741
wangxiaodi <wxd@choicesoft.com.cn>
2019-03-20 17:08:26 +0800

    scmk 版本升到 1.1.7

--- bin/logo.js
+++ bin/logo.js
@@ -1,20 +1,20 @@
 const chalk = require('chalk');
 
 function showLogo() {
-    console.log(chalk.blue(' :@##########$- ;##########@:  -$####&.  :@####$-!##$-   -$##%. ')); 
-    console.log(chalk.blue(' :@##!          !###;          -$#####%  ;#####$-!##$-  !##@:  '));  
-    console.log(chalk.blue(' :@##;          %###;          -$##&$##; !#####$-!##%-.&##!   '));   
-    console.log(chalk.blue(' :@#########&! -$###;          -$##$:%#$;|#####$-!##@@##$-   '));    
-    console.log(chalk.blue(' :@##########$ .&###;          -$##$--&#&$#####$-!@@###!     '));    
-    console.log(chalk.blue('           !#$ :@###;          -$##$- :@###$$##$-!@@#%%#$-    '));   
-    console.log(chalk.blue('           !#$ ;####;          -$##$-  !##$!$##$-!###! :@#|   '));   
-    console.log(chalk.blue('           !#$ !####;          -$##$-  .%#! $##$-!###!   !##;   ')); 
-    console.log(chalk.blue('           !#$ %##########@&%| -$##$-   .|  $##$-!###!    -%@&.  '));
-    console.log(chalk.blue('           !#$ !%%%%%277%%%%%%:.$##$-       $##$-!###!      :&#%-'));
-    console.log(chalk.blue('&#############                             :!!: -!!!-        |@$'));
-    console.log(chalk.blue('————————————————————————————————————————————————————————————————'));    
-    console.log('https://github.com/KnoveZ/scmk      SCMK   '+ require('../package.json').version +'    By Knove'); 
-    console.log(chalk.blue('————————————————————————————————————————————————————————————————'));    
+  console.log(chalk.blue(' :@##########$- ;##########@:  -$####&.  :@####$-!##$-   -$##%. '));
+  console.log(chalk.blue(' :@##!          !###;          -$#####%  ;#####$-!##$-  !##@:  '));
+  console.log(chalk.blue(' :@##;          %###;          -$##&$##; !#####$-!##%-.&##!   '));
+  console.log(chalk.blue(' :@#########&! -$###;          -$##$:%#$;|#####$-!##@@##$-   '));
+  console.log(chalk.blue(' :@##########$ .&###;          -$##$--&#&$#####$-!@@###!     '));
+  console.log(chalk.blue('           !#$ :@###;          -$##$- :@###$$##$-!@@#%%#$-    '));
+  console.log(chalk.blue('           !#$ ;####;          -$##$-  !##$!$##$-!###! :@#|   '));
+  console.log(chalk.blue('           !#$ !####;          -$##$-  .%#! $##$-!###!   !##;   '));
+  console.log(chalk.blue('           !#$ %##########@&%| -$##$-   .|  $##$-!###!    -%@&.  '));
+  console.log(chalk.blue('           !#$ !%%%%%277%%%%%%:.$##$-       $##$-!###!      :&#%-'));
+  console.log(chalk.blue('&#############                             :!!: -!!!-        |@$'));
+  console.log(chalk.blue('————————————————————————————————————————————————————————————————'));
+  console.log(`https://github.com/KnoveZ/scmk      SCMK   ${require('../package.json').version}    By Knove&Dee`);
+  console.log(chalk.blue('————————————————————————————————————————————————————————————————'));
 }
 
 exports = module.exports = showLogo;
